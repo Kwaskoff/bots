8c48794f4c5099a6cc7920c28572aaac25355ece
vik <v.lateralus@gmail.com>
2017-10-06 20:21:44 +0300

    No golos-js or steem-js required!

--- BlockSnobbery/wssub.js
+++ BlockSnobbery/wssub.js
@@ -1,20 +1,44 @@
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://api.golos.cf');
+const redis = require("redis")
+const client = redis.createClient()
+const pub = redis.createClient()
 
-ws.on('open', function open() {
-  ws.send(JSON.stringify({ 
+const Send = operations => {
+let ops = []
+for(let op of operations){
+	ops.push(op.op)
+}
+
+let JSONops = JSON.stringify(ops)
+console.log(JSONops)
+return pub.publish("BlockOperations", JSONops);
+}
+
+
+ws.on('open', open = () => {
+ws.send(JSON.stringify({ 
             id: 1,
             method: 'call', 
-            "params": ["database_api","set_block_applied_callback",[0]]
+            "params": ["database_api","set_block_applied_callback",[0],]
         }));
-});
-
 ws.on('message', function incoming(raw) {
-	let data = JSON.parse(raw)
-	if (!data.params) return
+let data = JSON.parse(raw)
+if (data.method === "notice" && data.params){
+	
 	let hex = data.params[1][0].previous.slice(0,8)
-	console.log(parseInt(hex,16))
-
+	let height = parseInt(hex,16)+1
+	ws.send(JSON.stringify({ 
+            id: 2,
+            method: 'call', 
+            params: ["database_api","get_ops_in_block",[height,"false"]]
+       }));
+	
+}
+else if (data.id === 2){
+	
+	Send(data.result)
+	
+} 
 })
-
-
+});
