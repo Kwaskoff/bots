f60cca843c919ec13a29c9d0d5493c0025b7f8ca
vik <v.lateralus@gmail.com>
2017-11-24 00:26:36 -1000

    Create transfers.js

---
+++ tests - drafts/transfers.js
@@ -0,0 +1,64 @@
+const golos = require('golos-js')
+const fs = require('fs')
+golos.config.set('websocket','wss://ws.golos.io');
+let l = 2000
+let f = l
+let count = 0
+let transfers =[]
+const getStat = (f) => {
+    
+golos.api.getAccountHistory('robot', f, l, (err, result)=> {
+if (err){console.log(err)}
+for (name of result){
+let block = name[1].block
+let isTransfer = name[1].op[0] === 'transfer'
+
+if(isTransfer){
+
+            let findmemo = true//name[1].op[1]['memo'][0] == ""
+			if (block > 0 && findmemo){
+			
+			let from = name[1].op[1]['from']
+			let to = name[1].op[1]['to']
+			let memo = name[1].op[1]['memo']
+			let amount = name[1].op[1]['amount']
+			
+		 let out = `${from} to ${to} ${amount}: ${memo}`
+			transfers.push(out)
+			console.log(out)
+             }
+			}
+		}
+	 
+let last = result[result.length - 1][0]
+if(last === f){
+		
+	f = f + l
+	getStat(f)
+
+	}
+	else if	(last < f) {
+	
+		
+		fs.appendFile('transfers.txt', transfers, (err)=> {
+		  if (err) {
+			console.log(err)
+		  } else {
+			
+		  }
+		  })
+			 
+			 l = 2000
+			 f = l
+			 accs = []
+			 count = 0
+			 accNum++
+			getStat(f)
+			
+		  
+	} 
+
+});
+
+}
+getStat(f)
