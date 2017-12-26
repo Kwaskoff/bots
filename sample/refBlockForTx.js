5ea45382da821ffce84e2f7cf04e25a3555065a2
vik <v.lateralus@gmail.com>
2017-12-25 23:02:06 -1000

    Get ref_block_num and ref_block_prefix

--- sample/refBlockForTx.js
+++ sample/refBlockForTx.js
@@ -1,9 +1,9 @@
 const golos = require("golos-js")
 golos.api.getDynamicGlobalProperties((err, result) => {
-  const refHeadNum = (result.last_irreversible_block_num - 1) & 0xFFFF;
-  steem.api.getBlockHeader(refHeadNum, function(e, res) {
-       const refId = new Buffer(res.previous, 'hex').readUInt32LE(4);
-       console.log(refHeadNum);
-       console.log(refId);
+  const BlockNum = (result.last_irreversible_block_num - 1) & 0xFFFF;
+  golos.api.getBlockHeader(refHeadNum, function(e, res) {
+       const BlockPrefix = new Buffer(res.previous, 'hex').readUInt32LE(4);
+       console.log(BlockNum);
+       console.log(BlockPrefix);
      });
 });
