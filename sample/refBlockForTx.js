56c05e6962529292a279295eaa15691bee006349
vik <v.lateralus@gmail.com>
2017-12-25 23:10:26 -1000

    Get ref_block_num and ref_block_prefix

--- sample/refBlockForTx.js
+++ sample/refBlockForTx.js
@@ -1,9 +1,16 @@
 const golos = require("golos-js")
+
 golos.api.getDynamicGlobalProperties((err, result) => {
+
   const BlockNum = (result.last_irreversible_block_num - 1) & 0xFFFF;
-  golos.api.getBlockHeader(refHeadNum, function(e, res) {
-       const BlockPrefix = new Buffer(res.previous, 'hex').readUInt32LE(4);
-       console.log(BlockNum);
-       console.log(BlockPrefix);
-     });
+    
+  golos.api.getBlockHeader(refHeadNum, function (e, res) {
+        
+    const BlockPrefix = new Buffer(res.previous, 'hex').readUInt32LE(4);
+        
+    console.log(BlockNum);
+    console.log(BlockPrefix);
+    
+  });
+  
 });
