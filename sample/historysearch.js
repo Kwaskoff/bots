a13aa87c9a2c7e6ac06dc0a000e33882ab98b147
vik <v.lateralus@gmail.com>
2018-08-12 15:55:05 +0300

    Update historysearch.js

--- sample/historysearch.js
+++ sample/historysearch.js
@@ -1,31 +1,16 @@
-// RUN IN BROWSER CONSOLE
+// GO to golos.cf and run this in browser console
 
-const ACC = "vp-liganovi4kov"
-
-golos.config.set('websocket', "wss://ws.golos.io")
-let limitcount = 2e3
-let fromcount = limitcount
-
-let seq = 1
-
-const getStat = (f) => {
-
-    golos.api.getAccountHistory(ACC, fromcount, limitcount, (err, result) => {
-        if (err) {
-            console.log(err)
-        }
-        for (name of result) {
-            let block = name[1].block
-            let TYPE = name[1].op[0] === 'comment'
-
-            if (TYPE) {
-              
-                
-                const KEY = name[1].op[1].parent_author === "" && JSON.parse(name[1].op[1].json_metadata).tags.includes('anketa-ln')
-
-                if (KEY) {
-                 
-                 golos.api.getContent(name[1].op[1].author,name[1].op[1].permlink, 0, (e,l)=>{
+var ACC = "vp-liganovi4kov"
+var limitcount = 2e3
+var fromcount = limitcount
+var seq = 1
+function getStat(f){
+golos.api.getAccountHistory(ACC, fromcount, limitcount, function(err, result){
+        if (err) return console.log(err)
+        for (var name of result) {
+            if (name[1].op[0] === 'comment') {
+                if (name[1].op[1].parent_author === "" && JSON.parse(name[1].op[1].json_metadata).tags.includes('anketa-ln')) {
+                 golos.api.getContent(name[1].op[1].author,name[1].op[1].permlink, 0, function(e,l){
                     if(e)return console.log(e) 
                     if(l.created===l.last_update){
                             console.log(seq,l.permlink,l.created)
@@ -39,20 +24,12 @@ const getStat = (f) => {
 
         let last = result[result.length - 1][0]
         if (last === f) {
-
-            fromcount = fromcount + limitcount
+			fromcount = fromcount + limitcount
             getStat(fromcount)
 
         } else if (last < fromcount) {
-
-
-        console.log('DONE')
-
-
-
-        }
-
-    });
-
+ console.log('DONE')
+}
+});
 }
 getStat(fromcount)
